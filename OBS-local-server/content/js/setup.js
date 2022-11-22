c469b1f6a5bc176794f9d228e9d08f18c3f284ba
Ethan Hammond <ethan_hammond@msn.com>
2022-11-21 20:39:34 -0500

    Add command item template, style, and javascript parser

--- OBS-local-server/content/js/setup.js
+++ OBS-local-server/content/js/setup.js
@@ -1,27 +1,61 @@
 /** @format */
 
 let converter;
+let command_template = null;
 
 const setInformationContents = (html) => {
   const info = document.getElementById("information-contents");
   info.innerHTML = html;
 };
 
-const getInformationContents = () => {
-  let result = null;
-  let xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", "/markdown/information.md", false);
-  xmlhttp.send();
-  if (xmlhttp.status == 200) {
-    result = xmlhttp.responseText;
+addCommand = async (name, command, usableBy) => {
+  if (null === command_template) {
+    await getTemplateContents();
   }
-  return result;
+  let contents = command_template;
+  contents = contents
+    .replaceAll("{name}", name)
+    .replaceAll("{command}", command)
+    .replaceAll("{usableBy}", usableBy);
+  domContent = $.parseHTML(contents);
+  $(domContent)
+    .find(`.select-usableby option[value="${usableBy}"]`)
+    .prop("selected", true);
+  $("#command-list").append(domContent);
 };
 
-const setupPage = () => {
+const getInformationContents = async () => {
+  return fetch("/markdown/information.md", {
+    headers: {
+      "Content-Type": "text/plain",
+    },
+  }).then((res) => {
+    return res.text().then((txt) => {
+      return txt;
+    });
+  });
+};
+
+const getTemplateContents = async () => {
+  return fetch("/html_templates/command-item.html", {
+    headers: {
+      "Content-Type": "text/plain",
+    },
+  }).then((res) => {
+    return res.text().then((txt) => {
+      command_template = txt;
+      return;
+    });
+  });
+};
+
+const setupPage = async () => {
   converter = new showdown.Converter();
-  let html = converter.makeHtml(getInformationContents());
+  let html = converter.makeHtml(await getInformationContents());
   setInformationContents(html);
+
+  // For debugging/dev, intented to be removed shortly
+  addCommand("!command", "Hello world from planet {planet++}", "everyone");
 };
 
 document.addEventListener("DOMContentLoaded", function () {
