ca678f677039de700710f64ab1afba50c0f04ae2
Ethan Hammond <ethan_hammond@msn.com>
2022-11-25 19:49:41 -0500

    Pass fancy command tests, whoo!

--- OBS-local-server/test/FancyCommandListener.test.ts
+++ OBS-local-server/test/FancyCommandListener.test.ts
@@ -6,53 +6,81 @@ import { expect } from "chai";
 import { io, io as SocketClient, Socket } from "socket.io-client";
 import { Server as SocketServer } from "socket.io";
 import "mocha";
+import { pino } from "pino";
 import { client } from "tmi.js";
+const logger = pino({"level": "debug"},pino.destination({"mkdir": true, "writable": true, "dest": `${__dirname}/logs/FancyCommandParser.test.log`}));
+
 
 const end_point = "http://localhost:8081";
 const opts = { forceNew: true, reconnect: true };
+
+function success(
+  done: Function,
+  io: SocketServer,
+  ...clients: Socket[]
+) {
+  io.close();
+  clients.forEach((client) => client.disconnect());
+  done();
+}
 
 describe("FancyCommandListener listener", () => {
   let IO: SocketServer;
   let FCE: FancyCommandListener;
   let client_io: Socket;
-  before(async () => {
+  after(()=>{
+    try {
+      client_io.close();
+      IO.close();
+    } catch {}
+  });
+  // Generally can be ready
+  it("Should be able to connect", (done) => {
+    logger.debug({"test": "Should be able to connect"},"Start");
     IO = new SocketServer();
-    FCE = new FancyCommandListener(IO);
+    FCE = new FancyCommandListener(IO, true);
     IO.listen(8081);
-    client_io = SocketClient(end_point, opts);
-    client_io = client_io.connect();
-    await new Promise((res) => {
-      client_io.on("connect", () => res(true));
-    });
-  });
-  after(async () => {
-    IO.close();
-    client_io.close();
-  });
-  describe("General readieness", () => {
-    it("Should be able to connect", (done) => {
-      client_io.connect();
+    client_io = SocketClient(end_point, opts);    
+    client_io.on("connect", () => {
+      logger.debug({"test": "Should be able to connect"},"Client connection detected");
       expect(client_io.connected).to.be.true;
-      done();
+      logger.debug({"test": "Should be able to connect"},`IO connected status ${client_io.connected}`);      
+      success(done, IO, client_io);
+      logger.debug({"test": "Should be able to connect"},"Done");
     });
+    client_io.connect();
   });
 
-  describe("Adding commands", () => {
+  it("Should be able to recieve and reply to add-command events", (done) => {
     const add_command: object = {
       name: "!test",
       command: "Some command",
       usableBy: "everyone",
     };
+    logger.debug({"test": "Should be able to recieve and reply to add-command events"},"Start");
+    IO = new SocketServer();
+    FCE = new FancyCommandListener(IO, true);
+    IO.listen(8081);
+    client_io = SocketClient(end_point, opts);    
+    
+    client_io.on("connect", () => {
+      logger.debug({"test": "Should be able to recieve and reply to add-command events"},"Connection recieved, emitting add-command");
+      client_io.emit("add-command", add_command);      
+    });
 
-    it("Should be able to recieve and reply to add-command events", (done) => {
-      IO.on("add-command", (res) => {
-        expect(res).to.equal(add_command);
-        client_io.emit("add-command", add_command);
-        done();
+    IO.on("connection", (srv_socket) => {
+      logger.debug({"test": "Should be able to recieve and reply to add-command events"},"Server connection established");
+      srv_socket.on("add-command", (res) => {
+        logger.debug({"test": "Should be able to recieve and reply to add-command events"},"add-command recieved from client");
+        expect(res).to.eql(add_command);        
+        success(done, IO, client_io);
+        logger.debug({"test": "Should be able to recieve and reply to add-command events"},"Done");
       });
     });
-    // it("Should be able respond to add-command events", () => {
-    //   expect(add_command_responded).to.be.true;
-    // });
+
+    client_io = client_io.connect();    
   });
+  // it("Should be able respond to add-command events", () => {
+  //   expect(add_command_responded).to.be.true;
+  // });
 });
