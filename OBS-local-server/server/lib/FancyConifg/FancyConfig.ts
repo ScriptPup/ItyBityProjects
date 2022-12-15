95a8a43b9ab4c72cc1f44a0f610563b331382fdb
Ethan Hammond <ethan_hammond@msn.com>
2022-12-14 21:42:14 -0500

    Don't overwrite actual password value with stars if form is saved while password is *****

--- OBS-local-server/server/lib/FancyConifg/FancyConfig.ts
+++ OBS-local-server/server/lib/FancyConifg/FancyConfig.ts
@@ -29,28 +29,40 @@ export const FancyConfig = (socket: Socket): Socket => {
       configDB.ref("twitch-bot-acct").remove();
       return;
     }
+    // If a "ofuscated" password is provided, then don't overwrite the old password
+    if (acct["password"] === "*****") {
+      configDB.ref("twitch-bot-acct").get((ss: DataSnapshot) => {
+        acct["password"] = ss.val()["password"];
+        configDB.ref("twitch-bot-acct").set(acct);
+        acct["password"] = "*****";
+        socket.emit("get-bot-acct", acct);
+      });
+      return;
+    }
     configDB.ref("twitch-bot-acct").set(acct);
-    configDB.ref("twitch-bot-acct").get((ss: DataSnapshot) => {
-      socket.emit("get-bot-acct", ss.val());
-    });
+    sendTwitchBotConfig(socket);
   });
 
   // Request the stored data about the bot account details
   socket.on("get-bot-acct", () => {
     logger.debug("Recieved bot account get request from client");
-    configDB.ref("twitch-bot-acct").get((ss: DataSnapshot) => {
-      const f_acct = ss.val();
-      if (f_acct) {
-        if (f_acct["password"].length > 0) {
-          f_acct["password"] = "*****";
-        }
-      }
-      logger.debug(
-        { acct: f_acct },
-        "Recieved bot account get request from client"
-      );
-      socket.emit("get-bot-acct", f_acct);
-    });
+    sendTwitchBotConfig(socket);
   });
   return socket;
+};
+
+const sendTwitchBotConfig = (socket: Socket) => {
+  configDB.ref("twitch-bot-acct").get((ss: DataSnapshot) => {
+    const f_acct = ss.val();
+    if (f_acct) {
+      if (f_acct["password"].length > 0) {
+        f_acct["password"] = "*****";
+      }
+    }
+    logger.debug(
+      { acct: f_acct },
+      "Recieved bot account get request from client"
+    );
+    socket.emit("get-bot-acct", f_acct);
+  });
 };
