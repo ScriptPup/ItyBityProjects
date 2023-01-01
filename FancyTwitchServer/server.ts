b733109b5e07e16462c697a6d1e792178410a341
Ethan Hammond <ethan_hammond@msn.com>
2023-01-01 16:25:10 -0500

    Add FancyRedemptionListener to the main server

--- FancyTwitchServer/server.ts
+++ FancyTwitchServer/server.ts
@@ -9,6 +9,8 @@ import { startSimpleHTTP } from "./server/simple_static_filehost";
 import { FancyCommandListener } from "./server/lib/FancyCommandExecutor/FancyCommandListener";
 import { FancyConfig } from "./server/lib/FancyConifg/FancyConfig";
 import { FancyCommandExecutor } from "./server/lib/FancyCommandExecutor/FancyCommandExecutor";
+import { FancyRedemptionStorage } from "./server/lib/FancyCommandExecutor/FancyRedemptionStorage";
+import { FancyRedemptionListener } from "./server/lib/FancyCommandExecutor/FancyRedemptionListener";
 
 console.log(`Starting in env ${process.env.NODE_ENV}`);
 
@@ -19,6 +21,10 @@ const io: socketServer = ServeTwitchChat(http);
 // Setup a command listener which will handle incoming requests to modify commands stored in the local DB
 const FCE: FancyCommandExecutor = new FancyCommandExecutor();
 const FCL: FancyCommandListener = new FancyCommandListener(io, FCE);
+// Setup a redemption listener which will handle incoming requests to modify redemptions stored in the local DB
+const FRS: FancyRedemptionStorage = new FancyRedemptionStorage();
+const FRL: FancyRedemptionListener = new FancyRedemptionListener(io, FRS);
+
 // Configure basic twitch listener
 const TL: TwitchListener = new TwitchListener(FCL);
 // Setup socket to listen for bot account commands and inform the TwitchListener when bot account information is changed
