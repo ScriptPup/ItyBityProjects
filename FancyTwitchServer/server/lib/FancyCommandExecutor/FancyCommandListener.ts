6a9899d4ad09a5ada24c45d940bb6dd0d2d951f1
Ethan Hammond <ethan_hammond@msn.com>
2023-01-01 11:31:16 -0500

    Implement protected property to define listener prefix

--- FancyTwitchServer/server/lib/FancyCommandExecutor/FancyCommandListener.ts
+++ FancyTwitchServer/server/lib/FancyCommandExecutor/FancyCommandListener.ts
@@ -3,4 +3,6 @@
 import { FancyCommand } from "../../../shared/obj/FancyCommandTypes";
 import { FancyListener } from "./FancyListener";
 
-export class FancyCommandListener extends FancyListener<FancyCommand> {}
+export class FancyCommandListener extends FancyListener<FancyCommand> {
+  protected override evtPrefix: string = "command";
+}
