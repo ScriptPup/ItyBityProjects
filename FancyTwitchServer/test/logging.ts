e412ad2a53dab5157ea5b747f430f37d7ce3a943
Ethan Hammond <ethan_hammond@msn.com>
2023-01-01 11:30:37 -0500

    Break out loggers so the test logger is in the tests folder

---
+++ FancyTwitchServer/test/logging.ts
@@ -0,0 +1,23 @@
+/** @format */
+
+import { pino } from "pino";
+
+const LOGGING_LEVEL = process.env.NODE_ENV === "development" ? "debug" : "info";
+
+export const TestLogger = pino(
+  {
+    level: LOGGING_LEVEL,
+    redact: [
+      "*.password",
+      "twitchClient.opts.identity.password",
+      "*.client_secret",
+      "*.auth_code",
+    ],
+  },
+  pino.destination({
+    mkdir: true,
+    writable: true,
+    dest: `${__dirname}/../../test/logs/tests.log`,
+    append: false,
+  })
+);
