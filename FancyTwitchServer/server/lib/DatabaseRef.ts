0e82759102bff2a136d0af3a62100880bf2b2de4
Ethan Hammond <ethan_hammond@msn.com>
2023-01-08 21:05:44 -0500

    Get stuff prepped for showcase redemptions

--- FancyTwitchServer/server/lib/DatabaseRef.ts
+++ FancyTwitchServer/server/lib/DatabaseRef.ts
@@ -25,3 +25,11 @@ export const commandVarsDB = new AceBase("variableDB", {
   logLevel: "error",
   info: "",
 }); // Sponsors can turn off the logo. I did a one-time sponsor of the AceBase project because it's great! However I don't have the $$ to keep a monthly sponsorship going and don't feel good about entirely removing the "advert" for a measly few bucks based on how much work the author puts in. Instead, I'm just going to turn off the banner for development since the banner makes the console difficult to parse. Will have it on in production.
+
+// Showcase connection used to show off art, given an order
+console.log("--- showcaseDB ---");
+export const showcaseDB = new AceBase("showcaseDB", {
+  sponsor: process.env.NODE_ENV === "development",
+  logLevel: "error",
+  info: "",
+}); // Sponsors can turn off the logo. I did a one-time sponsor of the AceBase project because it's great! However I don't have the $$ to keep a monthly sponsorship going and don't feel good about entirely removing the "advert" for a measly few bucks based on how much work the author puts in. Instead, I'm just going to turn off the banner for development since the banner makes the console difficult to parse. Will have it on in production.
---
+++ FancyTwitchServer/server/lib/FancyCommandParser/middlewares/ImgShowcaseAssignment.ts
@@ -0,0 +1,78 @@
+/** @format */
+
+import { Next, FancyCommandParser } from "../FancyCommandParser";
+import { TwitchMessage } from "../../../../shared/obj/TwitchObjects";
+import { MainLogger } from "../../logging";
+
+const logger = MainLogger.child({ file: "ImgShowcaseAssignment" });
+
+/**
+ * ImgShowcaseAssignment middleware, given a filename, will add to the showcase assignment list
+ *
+ * @remarks
+ * Implements <FancyMiddleware>
+ *
+ * @param FCP - Expects the FancyCommandParser object which will be used
+ * @param tmsg - Expects the twitch message object passed with the keys {message: string, tags: object, channel: string}
+ * @returns returns the FCP, which has the new middleware added on
+ *
+ */
+export function ImgShowcaseAssignment(
+  FCP: FancyCommandParser
+): FancyCommandParser {
+  const twitchFancyPreParse = (
+    context: { val: string },
+    next: Next,
+    tmsg: TwitchMessage
+  ): void => {
+    if (!tmsg) {
+      // This is debug instead of error, as it seems a "blank" message fires every so often.
+      // This doesn't appear to be a bug, just a weird quirk of tmi.js?
+      logger.debug({ tmsg }, "Cannot make replacements, no data provided!");
+      return;
+    }
+    try {
+      context.val = context.val.replace(/\@channel/gi, tmsg.channel);
+    } catch (err) {
+      logger.error({ tmsg: tmsg, err }, "Failed to replace @channel");
+    }
+
+    if (tmsg.userInfo.displayName) {
+      try {
+        const username = tmsg.userInfo.displayName || "unknown";
+        context.val = context.val.replace(/\@user/gi, username);
+      } catch (err) {
+        logger.error({ tmsg: tmsg, err }, "Failed to replace @user");
+      }
+    } else {
+      logger.error({ tmsg }, "Failed to replace @user");
+    }
+
+    try {
+      context.val = context.val.replace(/\@message/gi, tmsg.message);
+    } catch (err) {
+      logger.error({ tmsg: tmsg, err }, "Failed to replace @message");
+    }
+
+    const paramPattern = /(?<pos_param>\@[0-9]+)/dgim;
+    const paramReplacements: IterableIterator<RegExpMatchArray> =
+      context.val.matchAll(paramPattern);
+    const msgWords = tmsg.message.split(" ");
+    for (const paramMatch of paramReplacements) {
+      logger.debug({ paramMatch }, `Replacing positional param`);
+      const param: string | undefined = paramMatch.groups?.pos_param;
+      if (!param) {
+        continue;
+      }
+      const paramIX = Number.parseInt(param.replace("@", ""));
+      try {
+        context.val = context.val.replace(param, msgWords[paramIX - 1]);
+      } catch (err) {
+        logger.error({ tmsg, err }, `Failed to replace @${paramIX - 1}`);
+      }
+    }
+    next();
+  };
+  FCP.preParse(twitchFancyPreParse);
+  return FCP;
+}
