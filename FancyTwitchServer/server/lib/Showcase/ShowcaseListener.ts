35a2cc6f00b28f0056fe5feb35cc7089a196355c
Ethan Hammond <ethan_hammond@msn.com>
2023-01-16 15:52:28 -0500

    Finish setting up showcase listeners for client-server communication regarding requesting new showcases

--- FancyTwitchServer/server/lib/Showcase/ShowcaseListener.ts
+++ FancyTwitchServer/server/lib/Showcase/ShowcaseListener.ts
@@ -8,19 +8,27 @@ const showcase = new Showcase();
 
 export const showcaseListener = (IO: Server): Server => {
   IO.on("connection", (socket: Socket): void => {
-    socket.on("join-showcase", () => {
+    socket.on("join-showcase", (pos: number) => {
       socket.join("showcase");
-      socket.on("show-art", async (pos: number) => {
-        const artRedemption: ShowcaseItem =
-          (await showcase.getArtShowcaseRedeem(pos)) || {
-            redeemed_by: "Streamer",
-            redemption_name: "not_found.png",
-            redemption_thanks: "",
-            redemption_time: new Date(),
-          };
-        socket.emit("show-art", artRedemption);
+      showcase.onShowcaseAdd(async () => {
+        sendArt(pos, socket);
+      });
+      socket.on("show-art", async () => {
+        sendArt(pos, socket);
       });
     });
   });
   return IO;
+};
+
+const sendArt = async (pos: number, socket: Socket) => {
+  const artRedemption: ShowcaseItem = (await showcase.getArtShowcaseRedeem(
+    pos
+  )) || {
+    redeemed_by: "Streamer",
+    redemption_name: "not_found.png",
+    redemption_thanks: "",
+    redemption_time: new Date(),
+  };
+  socket.emit(`show-art-${pos}`, artRedemption);
 };
