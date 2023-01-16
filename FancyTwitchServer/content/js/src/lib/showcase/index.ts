8442fc838067e6f55699d8a9188a3faecdf56fe4
Ethan Hammond <ethan_hammond@msn.com>
2023-01-16 15:36:00 -0500

    Setup basic art showcase page

---
+++ FancyTwitchServer/content/js/src/lib/showcase/index.ts
@@ -0,0 +1,22 @@
+/** @format */
+
+import { ShowcaseClient } from "./ShowcaseClient";
+
+const startShowcase = () => {
+  const showcaseContainer: HTMLElement | null =
+    document.getElementById("showcase-container");
+  if (!showcaseContainer) {
+    throw "Showcase container wasn't found, and therefore the client can't be created.";
+  }
+  const params: URLSearchParams = new URLSearchParams(window.location.search);
+  const pos: Number = Number.parseInt(params.get("pos") || "0");
+  const client = new ShowcaseClient(showcaseContainer, pos);
+};
+
+if (document.readyState === "complete") {
+  startShowcase();
+} else {
+  document.addEventListener("DOMContentLoaded", function () {
+    startShowcase();
+  });
+}
